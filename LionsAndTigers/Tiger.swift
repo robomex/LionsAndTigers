fc8f91d0b1f5754dcf69539dd0567c48da3fd1aa
robomex <atmorales@gmail.com>
2014-09-28 10:39:02 -0500

    initial commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Tony Morales on 9/27/14.
+//  Copyright (c) 2014 Tony Morales. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
